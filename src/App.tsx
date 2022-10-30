06fe4e18884a8c79e72c8a89431784865dabb9c5
Chamara-Wijepala <mr.worf777@gmail.com>
2022-10-30 15:40:48 +0530

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <h1>Hello, World!</h1>;
-}
+import { Routes, Route } from "react-router-dom";
 
-export default App;
+import Home from "pages/home";
+import Shop from "pages/shop";
+import Product from "pages/product";
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/:id" element={<Product />} />
+      </Routes>
+    </div>
+  );
+}
