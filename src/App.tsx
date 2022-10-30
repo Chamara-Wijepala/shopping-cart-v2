75e9ef87f203e80c5d32c1c1b6d5e9112f8bc9e5
Chamara-Wijepala <mr.worf777@gmail.com>
2022-10-30 22:01:10 +0530

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 
+import Header from "components/header";
 import Home from "pages/home";
 import Shop from "pages/shop";
 import Product from "pages/product";
 
 export default function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/:id" element={<Product />} />
-      </Routes>
-    </div>
+    <>
+      <Header itemCount={0} />
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/:id" element={<Product />} />
+        </Routes>
+      </main>
+    </>
   );
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import clsx from "clsx";
+import { BiShoppingBag } from "react-icons/bi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+
+import "./header.css";
+
+export default function Header({ itemCount }: { itemCount: number }) {
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+
+  return (
+    <header className="bg-secondary-800">
+      <div className="container flex align-center">
+        <Link to="/">
+          <h1 className="fs-900 fw-400 clr-primary-100">
+            fake<span className="fw-800 clr-accent-400">store</span>
+          </h1>
+        </Link>
+
+        <nav
+          className={clsx(
+            isSideNavOpen && "open",
+            "primary-nav bg-secondary-800 flex"
+          )}
+        >
+          <NavLink to="/" className="nav-item fw-800 clr-primary-100" end>
+            HOME
+          </NavLink>
+          <NavLink to="/shop" className="nav-item fw-800 clr-primary-100">
+            SHOP
+          </NavLink>
+        </nav>
+
+        <button
+          type="button"
+          className="nav-item cart-btn fw-800 clr-primary-100 flex align-center"
+        >
+          <span className="sr-only">cart</span>
+          <BiShoppingBag className="cart-icon" /> {itemCount}
+          <span className="sr-only">items</span>
+        </button>
+
+        <button
+          type="button"
+          className="toggle-nav-btn nav-item clr-primary-100"
+          onClick={() => setIsSideNavOpen(!isSideNavOpen)}
+        >
+          {isSideNavOpen ? (
+            <MdClose className="toggle-nav-icon" />
+          ) : (
+            <GiHamburgerMenu className="toggle-nav-icon" />
+          )}
+        </button>
+      </div>
+    </header>
+  );
+}
