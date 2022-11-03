5ec0889f39e2b79b2161450bd9d7f530e161fb4d
Chamara-Wijepala <mr.worf777@gmail.com>
2022-11-03 20:24:51 +0530

    Add styles to shop page tab panel

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,16 +1,29 @@
 import { NavLink, Outlet } from "react-router-dom";
 
+import "./shop.css";
+
 export default function Shop() {
   return (
-    <>
-      <nav>
-        <NavLink to="all">All</NavLink>
-        <NavLink to="men">Men&apos;s Clothing</NavLink>
-        <NavLink to="women">Women&apos;s Clothing</NavLink>
-        <NavLink to="jewellery">Jewellery</NavLink>
+    <div className="container">
+      <nav
+        aria-label="secondary navigation"
+        className="category-tab-panel fw-800 no-list-style uppercase text-center"
+      >
+        <NavLink to="all" className="tab-item">
+          All
+        </NavLink>
+        <NavLink to="men" className="tab-item">
+          Men&apos;s Clothing
+        </NavLink>
+        <NavLink to="women" className="tab-item">
+          Women&apos;s Clothing
+        </NavLink>
+        <NavLink to="jewellery" className="tab-item">
+          Jewellery
+        </NavLink>
       </nav>
 
       <Outlet />
-    </>
+    </div>
   );
 }
